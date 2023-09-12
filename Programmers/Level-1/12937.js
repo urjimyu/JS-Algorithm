12920d58d3bd75e6591b3d8915518772e29bc375
urjimyu <92876819+urjimyu@users.noreply.github.com>
2023-09-12 22:33:52 +0900

    chores: 주석 제거

--- Programmers/Level-1/12937.js
+++ Programmers/Level-1/12937.js
@@ -4,12 +4,11 @@ function solution(num) {
 	return answer;
 }
 
-// 다른 풀이
-// 0은 false라는 것을 이용
-// function evenOrOdd(num) {
-//   return num % 2 ? "Odd" : "Even"
-// }
+// 다른 풀이 : 0은 false라는 것을 이용
+function evenOrOdd(num) {
+	return num % 2 ? "Odd" : "Even";
+}
 
-// // 아래는 테스트로 출력해 보기 위한 코드입니다.
-// console.log("결과 : " + evenOrOdd(2));
-// console.log("결과 : " + evenOrOdd(3));
+// 아래는 테스트로 출력해 보기 위한 코드입니다.
+console.log("결과 : " + evenOrOdd(2));
+console.log("결과 : " + evenOrOdd(3));
--- Programmers/Level-1/12944.js
+++ Programmers/Level-1/12944.js
@@ -8,10 +8,10 @@ function solution(arr) {
 }
 
 // reduce와 화살표함수를 사용한 다른 풀이
-// function average(array) {
-// 	return array.reduce((a, b) => a + b) / array.length;
-// }
+function average(array) {
+	return array.reduce((a, b) => a + b) / array.length;
+}
 
-// // 아래는 테스트로 출력해 보기 위한 코드입니다.
-// var testArray = [5, 3, 4];
-// console.log("평균값 : " + average(testArray));
+// 아래는 테스트로 출력해 보기 위한 코드입니다.
+var testArray = [5, 3, 4];
+console.log("평균값 : " + average(testArray));
