6eaf03e4926d50608e64e6e9609cf1cdd152da51
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-07-12 09:05:14 +0900

    solution: BOJ 1920 풀이(메모리 초과)

--- BOJ/class2/1920.js
+++ BOJ/class2/1920.js
@@ -1,16 +1,35 @@
 const fs = require("fs");
 const [N, A, M, nums] = fs
-	.readFileSync("/Users/jimyu/JS-Algorithm/BOJ/class2/test.txt")
+	.readFileSync("/dev/stdin")
 	.toString()
 	.trim()
 	.split("\n");
 
+function findM(num, arr) {
+	let tmpArr = [...arr];
+	let isNum = false;
+	let i = 1;
+	while (!isNum && i <= Math.ceil(Math.sqrt(arr.length))) {
+		let centerIdx = Math.ceil(tmpArr.length / 2) - 1;
+		let centerValue = tmpArr[centerIdx];
+		if (num === centerValue) isNum = true;
+		if (centerValue < num) tmpArr.splice(0, centerIdx + 1);
+		if (centerValue > num) tmpArr.splice(centerIdx + 1);
+		i++;
+	}
+	return isNum;
+}
+
 function findNum(A, nums) {
 	const answer = [];
 	const arrA = A.split(" ");
+	arrA.sort((a, b) => a - b);
 	const arrNums = nums.split(" ");
 
-	arrNums.forEach((nums) => answer.push(arrA.includes(nums) ? 1 : 0));
+	arrNums.forEach((num) => {
+		const isM = findM(num, arrA);
+		answer.push(isM ? 1 : 0);
+	});
 	return console.log(answer.join("\n"));
 }
 
