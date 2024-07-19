1e826454cec55b061a90b86a8cf04ce2995185cf
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-07-19 09:10:57 +0900

    solution: BOJ 10816(개선)

--- BOJ/class2/10816.js
+++ BOJ/class2/10816.js
@@ -7,14 +7,10 @@ const [n, cards, m, targets] = fs
 	.map((line) => line.split(" ").map(Number));
 
 function countCards(n, cards, m, targets) {
-	const answer = [];
 	const cardCounts = {};
-	cards.forEach((card) => (cardCounts[card] = 0));
-	cards.forEach((card) => (cardCounts[card] += 1));
+	cards.forEach((card) => (cardCounts[card] || 0) + 1);
 
-	for (let i = 0; i < m; i++) {
-		answer.push(cardCounts[targets[i]] ? cardCounts[targets[i]] : 0);
-	}
+const answer = targets.map((target) => cardCounts[target] || 0);
 	console.log(...answer);
 }
 
