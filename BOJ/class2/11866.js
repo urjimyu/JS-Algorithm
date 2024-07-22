74f624e5ab346ce5ea40f0876e5702d46551efd0
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-07-22 09:38:12 +0900

    solution: 11866 정답 풀이

--- BOJ/class2/11866.js
+++ BOJ/class2/11866.js
@@ -9,30 +9,23 @@ const [n, k] = fs
 
 function Josephus(n, k) {
 	const answer = [];
-	const arr = [];
-	let lastIdx = 0;
+	const queue = [];
+	let count = 1;
 
 	// 1 ~ N 채우기
 	for (let i = 1; i <= n; i++) {
-		arr.push(i);
+		queue.push(i);
 	}
 
-	for (let j = 1; j <= n; j++) {
-		// 남은 순회 횟수
-		const leftOver = arr.length - lastIdx - 1;
-
-		// 마지막 도착 지점 계산
-		if (k <= leftOver) targetIdx = lastIdx + k;
-		else if ((k - leftOver) % arr.length === 0) {
-			targetIdx = arr.length - 1;
+	// 큐를 이용해 정답 구하기
+	while (queue.length) {
+		const item = queue.shift();
+		if (count % k === 0) {
+			answer.push(item);
 		} else {
-			targetIdx = ((k - leftOver) % arr.length) - 1;
+			queue.push(item);
 		}
-		// 정답에 넣고 추출하기
-		answer.push(arr[targetIdx]);
-		arr.splice(targetIdx, 1);
-		// 멈춘 위치 저장
-		lastIdx = targetIdx;
+		count += 1;
 	}
 	console.log(`{${answer.join(", ")}}`);
 }
