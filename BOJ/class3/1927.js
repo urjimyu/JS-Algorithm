c567791564738fade2adb3cee6324952c99a1789
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-09-03 02:27:11 +0900

    240902 : 메모리초과

---
+++ BOJ/class3/1927.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const [n, ...inputs] = fs
+	.readFileSync("/dev/stdin")
+	.toString()
+	.trim()
+	.split("\n")
+	.map(Number);
+
+function solution(n, arr) {
+	let tmpArr = [];
+	let answer = [];
+
+	for (let i = 0; i < n; i++) {
+		if (arr[i] === 0) {
+			if (tmpArr.length === 0) answer.push(0);
+			else {
+				tmpArr.sort((a, b) => a - b);
+				let num = tmpArr.shift();
+				answer.push(num);
+			}
+		} else tmpArr.push(arr[i]);
+	}
+
+	return console.log(answer.join("\n"));
+}
+
+solution(n, inputs);
