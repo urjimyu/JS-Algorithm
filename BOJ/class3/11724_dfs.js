bfe9a5fda45add0473aebd1da88cde707bc98d6a
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-09-30 13:17:25 +0900

    240930: 11724 수정

--- BOJ/class3/11724_dfs.js
+++ BOJ/class3/11724_dfs.js
@@ -1,29 +1,31 @@
-const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
-const [n, m] = input.shift().split(" ").map(Number);
+const [info, ...arr] = require("fs")
+	.readFileSync("/dev/stdin")
+	.toString()
+	.trim()
+	.split("\n");
+const [n, _] = info.split(" ").map(Number);
+const graph = Array.from({ length: n + 1 }, () => []);
+const visited = new Array(n + 1).fill(false);
 
-input.map((line) => line.split(" ").map(Number));
-const visited = Array(n + 1).fill(false);
-const graph = [...Array(n + 1)].map(() => []);
-
-function solution(n, m, input) {
+function countComponent(arr) {
 	let count = 0;
-	// 양방향/무방향 그래프로 만들기
-	input.map(([from, to]) => {
+
+	const arr_info = arr.map((item) => item.split(" ").map(Number));
+	arr_info.map(([from, to]) => {
 		graph[from].push(to);
 		graph[to].push(from);
 	});
 
 	const dfs = (start) => {
-		for (const vertax of graph[start]) {
-			if (!visited[vertax]) {
-				visited[start] = true;
-				dfs(vertax);
+		visited[start] = true;
+		for (const node of graph[start]) {
+			if (!visited[node]) {
+				dfs(node);
 			}
 		}
 	};
 
-	for (let i = 1; i <= n; i++) {
+	for (let i = 1; i < n + 1; i++) {
 		if (!visited[i]) {
 			dfs(i);
 			count += 1;
@@ -32,4 +34,4 @@ function solution(n, m, input) {
 	console.log(count);
 }
 
-solution(n, m, input);
+countComponent(arr);
