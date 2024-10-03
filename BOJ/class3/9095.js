9f88b0f28b932d88c62ca3c7914ea732266f95e8
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-10-03 09:00:42 +0900

    241003: dp

--- BOJ/class3/9095.js
+++ BOJ/class3/9095.js
@@ -17,3 +17,32 @@ function sums(cases) {
 }
 
 sums(cases);
+
+// 복습2
+const [_, ...cases] = require("fs")
+	.readFileSync("./dev/stdin")
+	.toString()
+	.trim()
+	.split("\n")
+	.map(Number);
+
+const countCases = () => {
+	const max = Math.max(...cases);
+	const memo = Array.from({ length: max + 1 }, () => -1);
+	const answer = [];
+	memo[1] = 1;
+	memo[2] = 2;
+	memo[3] = 4;
+
+	for (let i = 4; i < max + 1; i++) {
+		memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3];
+	}
+
+	for (const num of cases) {
+		answer.push(memo[num]);
+	}
+	return answer;
+};
+
+const result = countCases();
+console.log(result.join("\n"));
