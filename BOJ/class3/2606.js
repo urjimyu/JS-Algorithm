a06bbe486ff96a0ef6029133f686ad01dbcf6306
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-10-14 08:34:00 +0900

    241014: dfs

--- BOJ/class3/2606.js
+++ BOJ/class3/2606.js
@@ -1,41 +1,35 @@
-const fs = require("fs");
-const [N, k, ...matches] = fs
-	.readFileSync("/dev/stdin")
+const [n, m, ...relations] = require("fs")
+	.readFileSync("./dev/stdin")
 	.toString()
 	.trim()
 	.split("\n");
 
-const N_num = Number(N);
-const k_num = Number(k);
+const rel = relations.map((v) => v.split(" ").map(Number));
 
-const graph = Array.from(Array(N_num + 1), () => []);
-const visited = Array.from(Array(N_num + 1), () => false);
-// 무조건 1번 컴퓨터에서 출발하므로
-visited[1] = true;
-let count = 0;
+const connected = Array.from({ length: +n + 1 }, () => []);
+const visited = Array.from({ length: +n + 1 }, () => 0);
 
-function dfs(start) {
-	// 시작 지점과 연결된 노드들 세기
-	for (let i of graph[start]) {
-		if (visited[i] === false) {
-			visited[i] = true;
-			count++;
-			dfs(i);
-		}
-	}
+let cnt = 0;
+
+for (let i = 0; i < +m; i++) {
+	const [a, b] = rel[i];
+	connected[a].push(b);
+	connected[b].push(a);
 }
 
-function countVirus(k, matches) {
-	// 서로 연결되어 있는 노드 입력
-	for (let i = 0; i < k; i++) {
-		const [first, second] = matches[i].split(" ").map(Number);
-		graph[first].push(second);
-		graph[second].push(first);
-	}
+const virus = () => {
+	const dfs = (start) => {
+		visited[start] = 1;
+		for (const node of connected[start]) {
+			if (visited[node] !== 1) {
+				cnt += 1;
+				dfs(node);
+			}
+		}
+	};
 
 	dfs(1);
-}
+	console.log(cnt);
+};
 
-countVirus(k_num, matches);
-
-console.log(count);
+virus();
